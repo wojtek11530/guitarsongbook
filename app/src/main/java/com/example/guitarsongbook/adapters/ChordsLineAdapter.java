b2d641535d9282b1f5f843da8481cc5d617beedd
wojtek11530 <wojtek19962a32@gmail.com>
2019-07-16 13:24:09 +0200

    Add displaying diagram with the fixed picture of C-dur chord and symbol of the chord after tapping a chord in lyric

--- app/src/main/java/com/example/guitarsongbook/adapters/ChordsLineAdapter.java
+++ app/src/main/java/com/example/guitarsongbook/adapters/ChordsLineAdapter.java
@@ -1,15 +1,21 @@
 package com.example.guitarsongbook.adapters;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.guitarsongbook.R;
 import com.example.guitarsongbook.model.Chord;
 
@@ -64,8 +70,26 @@ public class ChordsLineAdapter extends RecyclerView.Adapter<ChordsLineAdapter.Ch
                 @Override
                 public void onClick(View v) {
                     int position = getAdapterPosition();
-                    Toast toast = Toast.makeText(context, mChordsInLine.get(position).getMSymbol(), Toast.LENGTH_SHORT);
-                    toast.show();
+
+                    Chord chordToDisplay = mChordsInLine.get(position);
+
+                    AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                    View dialogView = mInflater.inflate(R.layout.dialog_chord_pics, null);
+
+                    TextView chordSymbolTextView = dialogView.findViewById(R.id.chord_in_dialog_symbol_txt_);
+                    chordSymbolTextView.setText(chordToDisplay.getMSymbol());
+
+                    ImageView chordPictureImageView = dialogView.findViewById(R.id.chord_picture_img_);
+                    int chordDiagramId = context.getResources().getIdentifier("c_diagram", "drawable", context.getPackageName());
+
+                    RequestOptions requestOptions = new RequestOptions()
+                            .fitCenter()
+                            .diskCacheStrategy(DiskCacheStrategy.ALL);
+                    Glide.with(context).load(chordDiagramId).apply(requestOptions).into(chordPictureImageView);
+
+                    builder.setView(dialogView);
+                    Dialog chordDialog = builder.create();
+                    chordDialog.show();
                 }
             });
         }
