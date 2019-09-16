5a02041c19aba125e9173acbeebddd50d391a6fd
wojtek11530 <wojtek19962a32@gmail.com>
2019-09-16 13:45:17 +0200

    refactor some code

--- app/src/main/java/com/example/guitarsongbook/fragments/SongListFragment.java
+++ app/src/main/java/com/example/guitarsongbook/fragments/SongListFragment.java
@@ -168,24 +168,4 @@ public class SongListFragment extends SearchViewFragment {
 
         return view;
     }
-
-    /*
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-
-        inflater.inflate(R.menu.main, menu);
-        // Get the SearchView and set the searchable configuration
-        SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        // Assumes current activity is the searchable activity
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
-        searchView.setIconifiedByDefault(true);
-        searchView.setSubmitButtonEnabled(true);
-
-        super.onCreateOptionsMenu(menu, inflater);
-    }
-    */
-
-
-
 }
